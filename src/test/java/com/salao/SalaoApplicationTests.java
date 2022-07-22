807307456bbf99a7c67305144cdf6ca4d7581cfa
Paulo Costa <pauloamcosta@MacBook-Pro-de-Paulo.local>
2022-07-22 15:18:19 -0300

    primeiro commit

---
+++ src/test/java/com/salao/SalaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
